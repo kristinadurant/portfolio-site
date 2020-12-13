a34ad06e9867dec18baa0d5dcdaad3e1d153388d
kristinadurant <kristinadurant.p@gmail.com>
2020-12-13 16:43:06 -0500

    adding header links

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
 const Navbar = () => {
     return (
         <header>
-            <Link to='/'>Home</Link>
+            <nav>
+                <ul>
+                    <li><HashLink smooth to={`#banner`}><i className="fas fa-home"></i></HashLink></li>
+                    <li><HashLink smooth to={`#about`}>ABOUT ME</HashLink></li>
+                    <li><HashLink smooth to={`#portfolio`}>PORTFOLIO</HashLink></li>
+                    <li><HashLink smooth to={`#contact`}>CONTACT</HashLink></li>
+                </ul>
+            </nav>           
         </header>
     )
 }
