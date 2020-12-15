235eaba305963653dad13241e701cfb186689aee
kristinadurant <kristinadurant.p@gmail.com>
2020-12-15 15:27:58 -0500

    added favicon and site title

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <header>
             <nav>
                 <ul>
-                    <li><HashLink smooth to={`#intro`}><i className="fas fa-home"></i></HashLink></li>
+                    <li><HashLink smooth to={`#intro`} className="logo"><span>kika</span><span>dev</span></HashLink></li>
                     <li><HashLink smooth to={`#about`}>ABOUT ME</HashLink></li>
                     <li><HashLink smooth to={`#portfolio`}>PORTFOLIO</HashLink></li>
                     <li><HashLink smooth to={`#contact`}>CONTACT</HashLink></li>
